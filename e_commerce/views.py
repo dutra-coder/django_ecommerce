410f7929b169905c21714b3012d9e329aa0e26e4
tendresse <dutra.tendresse@gmail.com>
2021-11-09 15:06:01 +0100

    put imports and insert in urlpatterns the about_page and contact_page in src/url.py, create functions about_page and contact_page using context variables in src/views.py and update .gitignore

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request) :
-    return render(request, "home_page.html",{})
+    context = {
+        "title": "Main Page",
+        "content": "Welcome to Main page"
+    }
+    return render(request, "home_page.html",context)
+
+def about_page(request) :
+    context = {
+        "title": "About Page",
+        "content": "Welcome to About page"
+    }
+    return render(request, "about_page.html",context)
+    
+def contact_page(request) :
+    context = {
+        "title": "Contact Page",
+        "content": "Welcome to Contact page"
+    }
+    return render(request, "contact_page.html",context)
