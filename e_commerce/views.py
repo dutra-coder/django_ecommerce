66e126bc5830049ddba0f788a62c1bd7ffca0b72
tendresse <dutra.tendresse@gmail.com>
2021-11-11 15:35:13 +0100

    update e_commerce/forms.py, e_commerce/urls.py, e_commerce/views.py, create and set template/auth/login.html and template/auth/register.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm, RegisterForm
 def home_page(request) :
     context = {
         "title": "Main Page",
@@ -31,3 +32,49 @@ def contact_page(request) :
     #     print(request.POST.get('email'))
     #     print(request.POST.get('message'))
     return render(request, "contact/view.html",context)
+
+def login_page(request) :
+    form = LoginForm(request.POST or None)
+    context = {
+        'form':form
+    }
+    print("User logged in")
+    #print(request.user.is_authenticated)
+
+    if form.is_valid():
+        print(form.cleaned_data)
+
+    username=form.cleaned_data.get("username")
+    password=form.cleaned_data.get("password")
+    user=authenticate(request, username=username, password=password)
+    print(user)
+    #print(request.user.is_authenticated)
+
+    if user is not None:
+        #print(request.user.is_authenticated)
+        login(request, user)
+        print("Valid login")
+
+        #Redirect to success page
+        return redirect("/")
+    else:
+        #Return an error message "Invalid Login"
+        print("Invalid login")
+
+    return render(request, "auth/login.html",context)
+
+User = get_user_model()
+def register_page(request) :
+    form = LoginForm(request.POST or None)
+    context = {
+        'form':form
+    }
+
+    if form.is_valid():
+        print(form.cleaned_data)
+        username=form.cleaned_data.get("username")
+        email=form.cleaned_data.get("email")
+        password=form.cleaned_data.get("password")
+        newuser = User.objects.create_user(username, email, password)
+        print(newuser)
+    return render(request, "auth/register.html",{})
