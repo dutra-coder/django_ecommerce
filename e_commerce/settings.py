759dafe368a2081fbb55c714661c98870c623e30
tendresse <dutra.tendresse@gmail.com>
2021-11-16 14:52:21 +0100

    insert STATIC_FILES_DIR in src/e_commerce/settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_FILES_DIR = {
+    os.path.join(BASE_DIR, "static_local")
+}
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
